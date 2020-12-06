8df0560897f76f16ee0d84dcaaa4f6688d35744c
Aaditya Sharma <accolite-asharma@evernote.com>
2020-12-06 14:50:48 +0530

    Demo - spring bean lifecycle

--- src/com/aaditya/learn/springdemo/TrackCoach.java
+++ src/com/aaditya/learn/springdemo/TrackCoach.java
@@ -2,8 +2,9 @@ package com.aaditya.learn.springdemo;
 
 import com.aaditya.learn.springdemo.services.Coach;
 import com.aaditya.learn.springdemo.services.FortuneService;
+import org.springframework.beans.factory.DisposableBean;
 
-public class TrackCoach implements Coach {
+public class TrackCoach implements Coach, DisposableBean {
 
     private FortuneService fortuneService;
 
@@ -21,5 +22,20 @@ public class TrackCoach implements Coach {
     @Override
     public String getDailyFortune() {
         return  "Ran a hard 5k with some good fortune!!!! " + fortuneService.getFortune();
+    }
+
+    // add an init method
+    public void doMyStartupStuff() {
+        System.out.println("Started up TrackCoach bean");
+    }
+
+    // add a destroy method
+    public void doMyCleanupStuff() {
+        System.out.println("Cleaned up TrackCoach bean");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("TrackCoach: inside destroymethod");
     }
 }
